1b872b383f631554ed7cc66b12ea37846016f4ab
amir ghaderian <amir@amirs-MacBook-Air.local>
2022-01-26 12:22:44 +0330

    first
    in componet.ts  i Why it gives an error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'fainal angular';
+  appName = "Angular Converter";
+
+  rialValue: number = 0;
+  usdValue: number = 0;
+  usdValueInput: string = "";
+
+  fetchPrice() {
+    fetch("https://api.tgju.online/v1/data/sana/json")
+      .then(result => {
+        return result.json();
+      })
+      .then(data => {
+        let usdItem = data.sana.data.find(i => i.slug === 'sana_buy_usd');
+        let usd = parseInt(usdItem.p);
+        
+        this.usdValue = usd;
+        this.rialValue = (parseInt(this.usdValueInput) | 0) * usd;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CourseComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
